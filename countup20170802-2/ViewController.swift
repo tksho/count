11a2a32103f9a70ff85e29900283d30bc89c2ed8
nttr <nttr@BI-23562.local>
2017-08-02 14:27:31 +0900

    カウントアプリです

--- countup20170802-2/ViewController.swift
+++ countup20170802-2/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var num: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus() {
+        num = num + 1
+
+        if num > 10 {
+            label.textColor = UIColor.red
+        }
+        
+        label.text = String(num)
+    }
+    @IBAction func minus() {
+        num = num - 1
+        if num <= 10 {
+            label.textColor = UIColor.blue
+        }
+
+        label.text = String(num)
+    }
+    @IBAction func clear() {
+        num = 0
+        label.text = String(num)
+    }
+    @IBAction func double() {
+        num = num * 2
+        label.text = String(num)
+    }
 
 }
 
