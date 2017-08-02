4b7ce8c5911d5367aceaa254cfe8a6c28dab5c84
nttr <nttr@BI-23562.local>
2017-08-02 14:41:20 +0900

    カウントが10以下になったら赤色に

--- countup20170802-2/ViewController.swift
+++ countup20170802-2/ViewController.swift
@@ -15,12 +15,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.textColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func plus() {
@@ -29,23 +28,25 @@ class ViewController: UIViewController {
         if num > 10 {
             label.textColor = UIColor.green
         }
-        
         label.text = String(num)
     }
     @IBAction func minus() {
         num = num - 1
         if num <= 10 {
-            label.textColor = UIColor.blue
+            label.textColor = UIColor.red
         }
-
         label.text = String(num)
     }
     @IBAction func clear() {
         num = 0
         label.text = String(num)
+        label.textColor = UIColor.red
     }
     @IBAction func double() {
         num = num * 2
+        if num > 10 {
+            label.textColor = UIColor.green
+        }
         label.text = String(num)
     }
 
